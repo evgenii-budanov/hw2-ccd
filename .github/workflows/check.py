48127908233a91f0bdaf12bf4602daa8771588a2
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2023-02-09 12:27:25 +0300

    Improve GitHub Actions

--- .github/workflows/check.py
+++ .github/workflows/check.py
@@ -8,6 +8,6 @@ with open(sys.argv[1]) as json_file:
     data = json.load(json_file)
 
     assert_almost_equal(data['ron'], 15, 0)
-    assert_almost_equal(data['ron_err'], 0.6, 1)
-    assert_almost_equal(data['gain'], 2.0, 1)
-    assert_almost_equal(data['gain_err'], 0.02, 2)
+    assert_almost_equal(data['ron_err'], 0.9, 1)
+    assert_almost_equal(data['gain'], 2.1, 1)
+    assert_almost_equal(data['gain_err'], 0.03, 2)
